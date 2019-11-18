26ebae4fa7c0feec70de9f01c474b707441203d0
taylorcyang <taylorcyang@tencent.com>
2019-11-18 11:56:28 +0800

    order

--- member.cpp
+++ member.cpp
@@ -28,6 +28,7 @@ TEST(Member, VariableInit) {
 TEST(Member, VariableInitWithDefault) {
     struct X {
         int a;
+        // default initializers. (C++11)
         bool b = true;
         void* c;
     };
@@ -59,4 +60,27 @@ TEST(Member, VariableInitWithPartialInit) {
     EXPECT_EQ(x->a, 1);
     EXPECT_EQ(x->b, false);
     EXPECT_EQ(x->c, nullptr);
+}
+
+TEST(Member, VariableInitDestructOrder) {
+    using namespace std;
+    struct A {
+        A() { cout << "A" << endl; }
+
+        ~A() { cout << "~A" << endl; }
+    };
+    struct B {
+        B() { cout << "B" << endl; }
+
+        ~B() { cout << "~B" << endl; }
+    };
+
+    struct X {
+        A a;
+        B b;
+
+        X() : b(), a() {}
+    };
+
+    X x;
 }
